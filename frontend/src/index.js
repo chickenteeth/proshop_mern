358f61577c33eb59c5ff43dd722b0534beb33233
Tristan Stutzman <tristan.stutzman@gmail.com>
2023-06-02 09:45:20 -0700

    Add product details screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
+  createRoutesFromElements(                                                                                                    
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route index={true} path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 )
